beb7884611718ff3bb1adb8f1fbeba3babd21e4c
JenniferPosso <moritasan417@gmail.com>
2016-03-09 06:45:53 -0500

    Topic 2: Design Patterns and OOP in JavaScript complete

--- 02-oop-inheritance/Movie.js
+++ 02-oop-inheritance/Movie.js
@@ -1,31 +1,18 @@
 var Movie = function() {
-	var module = {
-		title: '',
-		hashmap: '',
-		play: function() {
-			listener = new MovieObserver();
-			listener.playing(this.title);
-		},
-		stop: function() {
-			listener = new MovieObserver();
-			listener.stopped(this.title);
-		},
-		set: function(attr, value) {
-			this[attr] = value;
-		},
-		get: function(attr) {
-			return this[attr];
-		}
+	this.title = '';
+	this.hashmap = '';
+	this.play = function() {
+		listener = new MovieObserver();
+		listener.playing(this.title);
 	};
-
-	var MovieObserver = function() {
-		this.playing = function(title) {
-			console.log("playing " + title);
-		};
-		this.stopped = function(title) {
-			console.log("stopped " + title);
-		};
+	this.stop = function() {
+		listener = new MovieObserver();
+		listener.stopped(this.title);
 	};
-
-	return module;
+	this.set = function(attr, value) {
+		this[attr] = value;
+	};
+	this.get = function(attr) {
+		return this[attr];
+	};
 }
