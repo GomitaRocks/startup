37e21daa3f03475b74c6a797cb9b6d64d52885d9
JenniferPosso <moritasan417@gmail.com>
2016-03-20 17:15:28 -0500

    guide example handlebar

--- 04-backbone/handlebar.js
+++ 04-backbone/handlebar.js
@@ -1,26 +1,76 @@
-Handlebars.registerHelper('list', function(items, options) {
-  var out = "<ul>";
+$(document).ready(function(){
 
-  for(var i=0, l=items.length; i<l; i++) {
-    out = out + "<li>" + options.fn(items[i]) + "</li>";
-  }
+/*	var source = $("#first-template").html();
+	var template = Handlebars.compile(source);
 
-  return out + "</ul>";
+	var context = {
+		title: "All about Handlebars",
+		body: "<p>This is a post about &lt;p&gt; tags</p>"
+	}
+
+	var el_html = template(context);
+
+	$("#render_here").html(el_html);
+	$("#render_here_again").html(el_html);*/
+
+	var source	= $(".post").html();
+	var template = Handlebars.compile(source);
+
+	var context	= {
+		author: {
+			firstName: "Jennifer",
+			lastName: "Posso"
+		},
+		body: "Cool Handlebars",
+		comments: [{
+			author: {
+				firstName: "Diego",
+				lastName: "Cardona"
+			},
+			body: "Here is the 1st comment"
+		},		{
+			author: {
+				firstName: "Ana",
+				lastName: "Cardona",
+			},
+			body: "Here is the 2st comment"
+		}]
+	};
+
+	Handlebars.registerHelper("fullName", function(person) {
+		return person.firstName + " " + person.lastName;
+	});
+
+	var html = template(context);
+
+	$(".post").html(html);
+
+
+	var ctx = {
+		headers: [{
+			id: 1, items: "one"
+		}, {
+			id: 2, items: "two"
+		}, {
+			id: 3, items: "three"
+		}, {
+			id: 4, items: "four"
+		}]
+	};
+
+	var source = $("#selections").html();
+	var template = Handlebars.compile(source);
+	var el_html = template(ctx);
+	$("#selections").html(el_html);
+
+
+
+
+
+
+
+
+
+
+
 });
-
-var source   = $("#entry-template").html();
-var template = Handlebars.compile(source);
-
-var context = {
-  movies: [
-    {Name: "Yehuda", Year: "Katz", Description: "lalalalalalalalala"},
-    {Name: "Carl", Year: "Lerche", Description: "lalalalalalalalala"},
-    {Name: "Alan", Year: "Johnson", Description: "lalalalalalalalala"}
-  ]
-};
-
-var html = template(context);
-
-$("#render_here").html(html);	
-
-
